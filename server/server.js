df33d2e162f0768e2967e547a8e96c0c56da5b70
Maxence ROYER <maxence.royer@live.fr>
2015-04-24 00:06:25 +0200

    resolve problem server (maxence)

--- server/server.js
+++ server/server.js
@@ -8,6 +8,18 @@ app.get('/', function(req, res) {
     res.sendfile(html_dir + 'index.html');
 });
 
+app.get('/styles/bootstrap/bootstrap.min.css', function(req, res) {
+    res.sendfile(html_dir + '/styles/bootstrap/bootstrap.min.css');
+});
+
+app.get('/styles/appli/style.css', function(req, res) {
+    res.sendfile(html_dir + '/styles/appli/style.css');
+});
+
+app.get('/scripts/bootstrap/bootstrap.min.js', function(req, res) {
+    res.sendfile(html_dir + "/scripts/bootstrap/bootstrap.min.js");
+});
+
 app.listen(42000);
 
 console.log('~~ Lancement du serveur');
